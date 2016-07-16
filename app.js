c9c5fc14719e19defae1a242a9cc4321c157118f
John D. Storey <johndangerstorey@gmail.com>
2016-07-16 03:02:51 -0600

    updates app.js

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const upload = multer({ dest: path.join(__dirname, 'uploads') });
 /**
  * Load environment variables from .env file, where API keys and passwords are configured.
  */
-dotenv.load({ path: 'secret/.env.keys' });
+dotenv.load({ path: 'secret/keys' });
 
 /**
  * Controllers (route handlers).
