5f193e3877d43b52777a316edce46b559505c815
John D. Storey <johndangerstorey@gmail.com>
2016-07-16 03:14:55 -0600

    adds keys to s3

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const upload = multer({ dest: path.join(__dirname, 'uploads') });
 /**
  * Load environment variables from .env file, where API keys and passwords are configured.
  */
-dotenv.load({ path: 'secret/keys' });
+dotenv.load({ path: 'https://s3-us-west-2.amazonaws.com/story-time/keys.prod' });
 
 /**
  * Controllers (route handlers).
