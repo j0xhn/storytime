66b8a1409db55b1bcb61f85c3472e95eef1f76d7
John D. Storey <johndangerstorey@gmail.com>
2016-07-19 09:33:48 -0600

    adds story detail - broken

--- public/js/angularApp.js
+++ public/js/angularApp.js
@@ -1,15 +1,20 @@
 angular.module('miniApp', [
   'ngRoute',
   'miniApp.landingCtrl',
-  'miniApp.services'
+  'miniApp.services',
+  'miniApp.controllers'
 ]);
 angular.module('miniApp').config(function ($routeProvider) {
-    $routeProvider
-        .when('/', {
-            templateUrl: 'pages/landing/index.html',
-            controller: 'LandingCtrl'
-        })
-        .when('/detailTest', {
-            templateUrl: 'views/detailTest.html'
-        })
+  $routeProvider
+  .when('/', {
+    templateUrl: 'pages/landing/index.html',
+    controller: 'LandingCtrl'
+  })
+  .when('/detailTest', {
+    templateUrl: 'views/detailTest.html'
+  })
+  .when('/story/:storyId/:storyTitle', {
+    templateUrl: 'pages/story/story.html',
+    controller: 'StoryCtrl'
+  })
 });
