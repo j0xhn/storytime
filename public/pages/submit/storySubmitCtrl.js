f6f5bb8b3a7516e9efa5932ef6894ed487fd3099
John D. Storey <johndangerstorey@gmail.com>
2016-08-02 09:39:04 -0600

    saves story in db

--- public/pages/submit/storySubmitCtrl.js
+++ public/pages/submit/storySubmitCtrl.js
@@ -13,15 +13,13 @@ angular.module('controllers')
   //  End WYSIWY
 
   $scope.submitStory = function(story){
-    var sampleStory = '{"inputs":[{"title":"Enter Anything","$$hashKey":"object:26","keyword":"Sample"},{"title":"Same As Before","$$hashKey":"object:34","keyword":"Sample2"}],"html":"<p>minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. [ sample ], followed by [ sample2 ] should go there</p>","title":"Test 1","photoUrl":"www.testurl.com","price":"1","tags":"happy, sad, lonely,","shortDesc":"This is the best minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.","longDesc":"Here is a really long desc Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}';
-    var story = JSON.parse(sampleStory);
+    // var sampleStory = '{"inputs":[{"title":"Enter Anything","$$hashKey":"object:26","keyword":"Sample"},{"title":"Same As Before","$$hashKey":"object:34","keyword":"Sample2"}],"html":"<p>minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. [ sample ], followed by [ sample2 ] should go there</p>","title":"Test 1","photoUrl":"www.testurl.com","price":"1","tags":"happy, sad, lonely,","shortDesc":"This is the best minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.","longDesc":"Here is a really long desc Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}';
     var tagArray = story.tags.split(/[,]+/).filter(Boolean);
     story.tags = tagArray.map(function(x){ return x.trim(); })
     var newHtml = story.html.replace(/\[/g, '<b ng-bind="');
     story.html = newHtml.replace(/\]/g, '"></b>');
 
-    storiesService.postStory(story).then(function(error, res){
-      debugger;
+    storiesService.postStory(story).then(function(res){
       console.log(res)
     })
   }
