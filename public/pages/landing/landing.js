a6f025ae6d32cdfb3d78494566b8b65441bc7be4
John D. Storey <johndangerstorey@gmail.com>
2016-08-25 22:38:40 -0600

    adds in theatrejs story, more work on landing

--- public/pages/landing/landing.js
+++ public/pages/landing/landing.js
@@ -4,7 +4,22 @@ angular.module('directives')
 			restrict: 'EA',
 			replace: true,
       controller: function($scope, storiesService) {
-        // logic goes here
+         var theater = theaterJS();
+         theater
+           .addActor('values', { speed: 0.8, accuracy: 0.6 })
+           .addScene('values: be strong', 600)
+           .addScene('values: be successfull.', 800)
+           .addScene('values: get good grades', 700)
+           .addScene('values: eat healthy', 600)
+           .addScene('values: put on sunscreen', 800)
+           .addScene('values: be a good friend', 400)
+           .addScene('values: deal with a school bully', 900)
+           .addScene('values: I think you get the gist', 900)
+           .addScene('values: basically they are simple stories', 900)
+           .addScene('values: that have your child as the main star', 900)
+           .addScene('values: it is really cool, give it a try!', 900)
+           .addScene('values: get ready for your child to learn to', 900)
+           .addScene(theater.replay.bind(theater))
       },
 			templateUrl: '/pages/landing/landing.html',
 			scope: {}
