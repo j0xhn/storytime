861ed18cbd2ccabbdf7dacf8ab229c935cffe780
John D. Storey <johndangerstorey@gmail.com>
2016-09-04 01:25:33 -0500

    progress with checkout

--- public/pages/detail/detail.js
+++ public/pages/detail/detail.js
@@ -3,7 +3,7 @@ angular.module('directives')
   return {
     restrict: 'EA',
     replace: true,
-    controller: function($scope, $routeParams, storiesService, paymentService, userService) {
+    controller: function($scope, $routeParams, $rootScope, storiesService, paymentService, userService) {
       var promiseOfStory = storiesService.getSelectedStory($routeParams.storyId);
       $scope.primaryButtonText = 'Download';
 
@@ -22,6 +22,11 @@ angular.module('directives')
             singleUse: true,
             amount: 10.00,
             currency: 'USD'
+          },
+          onPaymentMethodReceived: function (result) {
+            // do logic here to send back to our server
+            // store user as being in vault and set autoPay
+            // as true on their user
           }
         });
       })
@@ -31,8 +36,11 @@ angular.module('directives')
           // download and take from autoPay
         } else {
           $scope.paymentState = 'pickType';
-          $scope.primaryButtonText = 'Unlimited Downloads $3/mo'
         }
+      }
+
+      $scope.showForm = function (paymentType) {
+        $scope.paymentState = 'showForm'
       }
     },
     templateUrl: '/pages/detail/detail.html',
