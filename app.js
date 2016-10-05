f830a3f1a9408dfb38f2551f93f084c3b7487f64
John D. Storey <johndangerstorey@gmail.com>
2016-10-04 23:00:09 -0600

    fix to facebook redirect

--- app.js
+++ app.js
@@ -153,7 +153,8 @@ app.get('/auth/instagram/callback', passport.authenticate('instagram', { failure
 });
 app.get('/auth/facebook', passport.authenticate('facebook', { scope: ['email', 'user_location'] }));
 app.get('/auth/facebook/callback', passport.authenticate('facebook', { failureRedirect: '/login' }), (req, res) => {
-  res.redirect(req.session.returnTo || '/');
+  // res.redirect(req.session.returnTo || '/');
+  res.redirect('/');
 });
 app.get('/auth/github', passport.authenticate('github'));
 app.get('/auth/github/callback', passport.authenticate('github', { failureRedirect: '/login' }), (req, res) => {
