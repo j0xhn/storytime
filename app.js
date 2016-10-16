b86c899bcc5165cccf7d99715d6aa43d351f06b3
John D. Storey <johndangerstorey@gmail.com>
2016-10-15 22:21:33 -0600

    adds local env variable, adds feedback option

--- app.js
+++ app.js
@@ -91,6 +91,7 @@ app.use((req, res, next) => {
 });
 app.use(lusca.xframe('SAMEORIGIN'));
 app.use(lusca.xssProtection(true));
+app.locals.env = process.env.NODE_ENV
 app.use((req, res, next) => {
   res.locals.user = req.user;
   res.locals.userJson = JSON.stringify(req.user);
