980170662737f67094104f4d9704beab5d6f8973
John D. Storey <johndangerstorey@gmail.com>
2016-10-22 09:13:31 -0600

    makes toggle input dynamic, finishes from the story side, stil need submit work

--- public/partials/toggleInput/toggleInput.js
+++ public/partials/toggleInput/toggleInput.js
@@ -14,8 +14,10 @@ angular.module('directives')
       // incase a model isn't passed in, just creates a temporary one
       const tempObject = {}
       $scope.ngModel = $scope.ngModel || tempObject.model;
+      $scope.status = 'active';
       $scope.toggleModel = function(){
         $scope.ngModel = !$scope.ngModel;
+        $scope.status = $scope.status === 'active' ? '' : 'active';
       }
 
     },
