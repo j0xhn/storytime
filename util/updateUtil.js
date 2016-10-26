3c4231292c787643b65f142aad413468c52b36bd
John D. Storey <johndangerstorey@gmail.com>
2016-10-25 22:23:55 -0600

    adds ability to backup mongodb, starts updateUtil

---
+++ util/updateUtil.js
@@ -0,0 +1,7 @@
+const Story = require('../models/Story');
+
+const updateUtil = {};
+updateUtil.giveAllInputsType = function(query){
+  console.log('giving all inputs type function called');
+};
+module.exports = updateUtil;
