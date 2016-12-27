23979801f3a0702569d9b2828284a906d1f2ceff
John D. Storey <johndangerstorey@gmail.com>
2016-12-27 08:53:04 -0700

    changes uppercase names

--- util/updateUtil.js
+++ util/updateUtil.js
@@ -1,10 +1,10 @@
 const Story = require('../models/Story');
 const User = require('../models/User');
 
-const updateUtil = {};
-updateUtil.updateStoryPrice = function(query){
+const UpdateUtil = {};
+UpdateUtil.updateStoryPrice = function(query){
   Story.find().forEach(function(story){
     story.price = story.price ? story.price : 0;
   });
 };
-module.exports = updateUtil;
+module.exports = UpdateUtil;
