f1469a7f7d17fadea7075350f5b76defbcf4c4da
John D. Storey <johndangerstorey@gmail.com>
2016-12-27 09:02:21 -0700

    adds logic check for loading enviorment from local

--- app.js
+++ app.js
@@ -18,12 +18,15 @@ const expressValidator = require('express-validator');
 const sass = require('node-sass-middleware');
 const multer = require('multer');
 const upload = multer({ dest: path.join(__dirname, 'uploads') });
-const UpdateUtil = require('/util/UpdateUtil')
+const UpdateUtil = require('./util/UpdateUtil')
 
 /**
  * Load environment variables from .env file, where API keys and passwords are configured.
  */
-dotenv.load({ path: './config/.env.example' });
+if (process.env.NODE_ENV != 'Production'){
+  console.log(process.env.NODE_ENV);
+  dotenv.load({ path: './config/.env.example' });
+}
 
 /**
  * Controllers (route handlers).
