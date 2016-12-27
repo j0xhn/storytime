81b64698518e4d0c299d1dccdeac98fb0e46e088
John D. Storey <johndangerstorey@gmail.com>
2016-12-27 08:55:25 -0700

    removes updateUtil

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const expressValidator = require('express-validator');
 const sass = require('node-sass-middleware');
 const multer = require('multer');
 const upload = multer({ dest: path.join(__dirname, 'uploads') });
-const UpdateUtil = require('./util/UpdateUtil')
+// const UpdateUtil = require('/util/UpdateUtil')
 
 /**
  * Load environment variables from .env file, where API keys and passwords are configured.
