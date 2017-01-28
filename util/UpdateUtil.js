4cfed0f5139efa7809f15903748d0c949f2a5b86
John D. Storey <johndangerstorey@gmail.com>
2017-01-27 17:07:05 -0700

    adds update utility for authorName

--- util/UpdateUtil.js
+++ util/UpdateUtil.js
@@ -11,15 +11,34 @@ UpdateUtil.updateStoryPrice = function(){
 };
 
 UpdateUtil.authorName = function(){
+  const updateObject = { $set: { authorName: 'Doctor Dogood' }};
+  let updateCount = 0;
+  let errorCount = 0;
+  Story.find(function(err, stories){
+    let totalCount  = stories.length;
+    stories.forEach(function(story){
+      if(story.authorName === 'Guest Author'){
+        updateCount++;
+        Story.update(
+          {_id: story._id},
+          updateObject,
+          (err, res) => {
+            if(err){errorCount++; console.error(err)}
+          }
+        );
+      }
+    })
+    console.log(`${updateCount}/${totalCount} stories updated with ${errorCount} errors`)
+  })
+};
 
-}
 UpdateUtil.updateUser = function(){
   const updateVersion = 1;
   const updateObject = { $set: { __v: updateVersion }};
 
   User.find(function(err, users){
     let updateCount = 0;
-    let successCount = 0;
+    let errorCount = 0;
     let totalCount  = users.length;
     users.forEach(function(user){
       if(!user.__v || user.__v !== updateVersion){
@@ -28,13 +47,12 @@ UpdateUtil.updateUser = function(){
           {_id: user._id},
           updateObject,
           (err, res) => {
-            if(err){console.error(err)}
-            else{ successCount++ }
+            if(err){errorCount++; console.error(err)}
           }
         );
       }
     })
-    console.log(`${updateCount}/${totalCount} users updated with ${successCount/updateCount || 100}% success`)
+    console.log(`${updateCount}/${totalCount} users updated with ${errorCount} errors`)
   })
 };
 
